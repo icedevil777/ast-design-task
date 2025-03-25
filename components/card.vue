99dc3b008ae23736d12888ee5f67ea8322f976de
Icedevil777 <icedevil777228@mail.ru>
2025-03-25 17:45:25 +0300

    # perfect img

--- components/card.vue
+++ components/card.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import cardPng from '../assets/img/card.png';
+
 const isOpen = ref(false);
 
 const handleMouseEnter = () => {
@@ -10,30 +12,52 @@ const handleMouseLeave = () => {
   isOpen.value = false;
   console.log('handleMouseLeave');
 };
+
+interface Product {
+  name: string;
+  brand: string;
+  image?: string;
+  newPrice: number;
+  url: string;
+}
+
+const props = defineProps<{
+  product: Product;
+}>();
+
+let imageUrl = ref(props.product.image);
+
+const handleImageError = () => {
+  imageUrl.value = cardPng;
+};
+
+const text = computed(() => {
+  return props.product.brand.length > 15 ? props.product.brand.slice(1, 20) + ' ...' : props.product.brand + ' ' + props.product.brand;
+});
 </script>
 
 <template>
-  <div class="p-5">
-    <div :class="{ 'my-after': isOpen }" class="w-[201px] cursor-pointer" v-on:mouseenter="handleMouseEnter" v-on:mouseleave="handleMouseLeave">
+  <div class="p-4 h-[360px]" v-on:mouseleave="handleMouseLeave">
+    <div :class="{ 'my-after': isOpen }" class="w-[201px] cursor-pointer" v-on:mouseenter="handleMouseEnter">
       <div class="relative">
-        <img src="../assets/img/card.png" />
+        <img v-if="product.image" class="object-cover h-[201px] w-[201px] rounded-xl" :src="imageUrl" @error="handleImageError" />
+        <img v-else class="object-cover h-[201px] w-[201px] rounded-xl" :src="cardPng" />
         <div class="absolute bottom-2 left-2 bg-white rounded-lg px-2">
           <span class="text-[#E0119D] font-[400] text-[14px]"> -35% </span>
         </div>
       </div>
       <div class="space-x-2 mt-2">
-        <span class="font-[700] text-[16px]">1 288 ₽</span>
+        <span class="font-[700] text-[16px]"> {{ product.newPrice }} ₽</span>
         <span class="font-[400] text-[12px] text-[#999999] line-through">1 610 ₽</span>
       </div>
-      <span class="font-[600] text-[12px]">TERRIS</span>
-      <div class="font-[400] text-[14px]">Цветные наклейки на клавиши пианино, один ...</div>
+      <span class="font-[600] text-[12px]">{{ product.name }}</span>
+      <div class="font-[400] text-[14px]">{{ text }}</div>
     </div>
-    <button v-show="isOpen" class="bg-black text-white rounded-lg px-5 py-1.5 font-[500] text-[14px] cursor-pointer mt-2" >В корзину</button>
+    <button v-show="isOpen" class="bg-black text-white rounded-lg px-5 py-1.5 font-[500] text-[14px] cursor-pointer mt-2">В корзину</button>
   </div>
 </template>
 
 <style scoped>
-
 .my-after::after {
   content: '';
   background-color: white;
--- pages/catalog.vue
+++ pages/catalog.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="myh-screen px-4 sm:p-8 space-y-8 mt-[92px]">
-    <h1 class="text-xl sm:text-[32px] font-[700]">Тестовая задача</h1>
-    <div class="flex flex-wrap relative">
-      <card class="relative" v-for="i in 10" />
+    <h1 class="text-xl sm:text-[32px] font-[700]">Каталог продуктов</h1>
+    <div v-if="data.products" class="flex flex-wrap relative">
+      <card class="relative" v-for="product in data.products.slice(0, 10)" :product="product" />
+    </div>
+    <div v-else-if="status === 'pending'" class="flex text-3xl w-full h-96 justify-center">
+      <span class="m-auto">Loading ...</span>
     </div>
   </div>
 </template>
-<script lang="ts">
-import card from './card';
+
+<script setup lang="ts">
+const { data, status, error, refresh } = await useFetch(() => '/api/products');
 </script>
 
 <style scoped>
